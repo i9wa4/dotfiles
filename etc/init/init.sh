bc55818b0524529fcb3df77c01fc776c3e8fefa8
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-03-14 02:39:09 +0900

    Changes to be committed:

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -7,15 +7,6 @@ script_path="${script_dir}"/"${script_basename}"
 cd "${script_dir}"
 start_time=$(date +%s.%N)
 
-# General
-sudo sed -i -e "s/^# deb-src/deb-src/" /etc/apt/sources.list
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y \
-  bc \
-  unzip \
-  zip
-
 # Symbolic Link
 rm -rf "{HOME}"/.config
 rm -rf "{HOME}"/.vim
@@ -39,67 +30,22 @@ fi
 } >> "${HOME}"/.bashrc
 . "${HOME}"/.my_bashrc
 
+# General
+sudo sed -i -e "s/^# deb-src/deb-src/" /etc/apt/sources.list
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y \
+  bc \
+  nkf \
+  ripgrep \
+  unzip \
+  zip
+
 # Git
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt update
 sudo apt upgrade -y
 bash git_config.sh
-
-# Vim
-# sudo sed -i -e "s/^# deb-src/deb-src/" /etc/apt/sources.list
-# sudo apt update
-sudo apt build-dep -y vim
-sudo apt install -y ripgrep
-cd /usr/local/src/
-if [ ! -d ./vim/ ]; then
-  sudo git clone https://github.com/vim/vim.git
-fi
-cd -
-
-# Neovim
-# https://github.com/neovim/neovim/wiki/Building-Neovim
-# sudo apt update
-sudo apt install -y \
-  autoconf \
-  automake \
-  cmake \
-  cmake \
-  curl \
-  doxygen \
-  g++ \
-  gcc \
-  gettext \
-  libtool \
-  libtool-bin \
-  ninja-build \
-  pkg-config \
-  unzip
-sudo apt install -y nkf
-sudo apt install -y shellcheck
-cd /usr/local/src/
-if [ ! -d ./neovim/ ]; then
-  sudo git clone https://github.com/neovim/neovim.git
-fi
-cd -
-
-# Deno
-if [ -z "$(which deno)" ]; then
-  curl -fsSL https://deno.land/install.sh | bash
-fi
-
-# SKK
-mkdir -p "${HOME}"/.skk/
-cd "${HOME}"/.skk/
-wget http://openlab.jp/skk/dic/SKK-JISYO.L.gz
-gzip -d SKK-JISYO.L.gz
-wget http://openlab.jp/skk/dic/SKK-JISYO.jinmei.gz
-gzip -d SKK-JISYO.jinmei.gz
-cd -
-
-# Script
-bash vim_build.sh
-bash nvim_build.sh
-bash win_update.sh
 
 end_time=$(date +%s.%N)
 wall_time=$(echo "${end_time}"-"${start_time}" | bc -l)
