2413bcaa4209d5459ed2c796ad2da0545a172ab5
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-03-25 23:19:54 +0900

    Changes to be committed:

--- bin/windows/win_update.bat
+++ bin/windows/win_update.bat
@@ -1,6 +1,7 @@
 @echo off
 @REM Run as user.
 
+set CODESNIP_DIR="%APPDATA%\Code\User\snippets"
 set WINTERM_DIR="%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 
 call :main
@@ -10,17 +11,20 @@ exit /b
     chcp 65001>nul
     cd /d %~dp0
 
-    copy /y ".\.gitignore"              "%USERPROFILE%"
-    copy /y ".\.wslconfig"              "%USERPROFILE%"
-    copy /y ".\.markdownlintrc"         "C:\work"
-    copy /y ".\jupytext.yaml"           "C:\work"
+    @REM %USERPROFILE%
+    copy /y "..\etc\.gitignore" "%USERPROFILE%"
+    copy /y "..\etc\.wslconfig" "%USERPROFILE%"
+
+    @REM C:\work
+    copy /y "..\etc\.markdownlintrc" "C:\work"
+    copy /y "..\etc\jupytext.yaml" "C:\work"
 
     @REM VSCode
-    rmdir /q /s "%APPDATA%\Code\User\snippets"
-    xcopy /e /i /y ".\.vim\vsnip\*"     "%APPDATA%\Code\User\snippets"
-    xcopy /e /i /y ".\VSCode\User\*"    "%APPDATA%\Code\User"
+    rmdir /q /s "%CODESNIP_DIR:"=%"
+    xcopy /e /i /y "..\vsnip\*" "%CODESNIP_DIR:"=%"
+    xcopy /e /i /y "..\VSCode\User\*" "%APPDATA%\Code\User"
 
     @REM Windows Terminal
     rmdir /q /s "%WINTERM_DIR:"=%"
-    xcopy /e /i /y ".\WindowsTerminal\LocalState" "%WINTERM_DIR:"=%"
+    xcopy /e /i /y "..\WindowsTerminal\LocalState" "%WINTERM_DIR:"=%"
 exit /b
