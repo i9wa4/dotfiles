bb5b7e6fe1eb4e5ca1bb304aeae1d7835e678a0c
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-04-06 01:16:22 +0900

    Changes to be committed:

--- bin/windows/pyvma_jl.bat
+++ bin/windows/pyvma_jl.bat
@@ -10,12 +10,12 @@ exit /b
     chcp 65001>nul
     cd /d %~dp0
 
-    xcopy /e /i /y ".\.jupyter" "%VENV_MYENV:"=%\share\jupyter"
+    rmdir /q /s "%USERPROFILE%\.ipynb_checkpoints"
+
+    xcopy /e /i /y "..\..\.jupyter" "%VENV_MYENV:"=%\share\jupyter"
     cd /d ".."
 
     call "%VENV_MYENV:"=%\Scripts\activate.bat"
     jupyter-lab^
         --FileCheckpoints.checkpoint_dir="%USERPROFILE%\.ipynb_checkpoints"
-
-    rmdir /q /s "%USERPROFILE%\.ipynb_checkpoints"
 exit /b
