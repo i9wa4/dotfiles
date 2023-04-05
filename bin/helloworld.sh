70a7e9a331bafab5583bce9fef6beb559a31c009
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-04-06 00:47:48 +0900

    Changes to be committed:

--- bin/helloworld.sh
+++ bin/helloworld.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+set -euo pipefail
+export LC_ALL=C
+script_basename="$(basename "$0")"
+script_dir="$(cd "$(dirname "$0")"; pwd)"
+script_path="${script_dir}"/"${script_basename}"
+cd "${script_dir}"
+start_time=$(date +%s.%N)
+
 echo "Hello, World!"
 sleep 1
 echo "Done."
+
+end_time=$(date +%s.%N)
+wall_time=$(echo "${end_time}"-"${start_time}" | bc -l)
+echo ["${script_path}"] WallTime: "${wall_time}" [s]
