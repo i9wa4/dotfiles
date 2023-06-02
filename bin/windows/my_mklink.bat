8c9a043965f79fca2a893c0039c9622c64310f51
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-06-02 16:42:58 +0900

    Changes to be committed:

--- bin/windows/my_mklink.bat
+++ bin/windows/my_mklink.bat
@@ -7,7 +7,7 @@ exit /b
 :main
     cd /d %~dp0
 
-    mklink /d "%OneDrive%\home\repo\fragment\md"  "C:\work\git\fragment\md"
+    mklink /d "%OneDrive%\home\repo\fragment\md" "C:\work\git\fragment\md"
 
     pause
 exit /b
