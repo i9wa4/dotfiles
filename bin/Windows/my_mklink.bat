70f1b94bd01402439de90527bc5f55b83f0b4b52
i9wa4 <127664533+19wa4@users.noreply.github.com>
2023-08-20 22:23:00 +0900

    Changes to be committed:

--- bin/Windows/my_mklink.bat
+++ bin/Windows/my_mklink.bat
@@ -7,9 +7,7 @@ exit /b
 :main
     cd /d %~dp0
 
-    mklink /d "%OneDrive%\home\repo\note\archive" "C:\work\git\note\archive"
-    mklink /d "%OneDrive%\home\repo\note\bin" "C:\work\git\note\bin"
-    mklink /d "%OneDrive%\home\repo\note\doc" "C:\work\git\note\doc"
+    @REM mklink /d "%OneDrive%\home\repo\note" "C:\work\git\note"
     @REM mklink /d "%USERPROFILE%\work" "C:\work"
     @REM mklink "%USERPROFILE%\local.vim" "C:\work\local.vim"
     mklink "C:\work\bookmark.md" "%OneDrive%\home\repo\work\bookmark.md"
