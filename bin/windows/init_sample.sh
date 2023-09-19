43e8a70054d8bf1995be2f440a0c0a0415d518de
i9wa4 <127664533+i9wa4@users.noreply.github.com>
2023-09-19 21:46:36 +0900

    Changes to be committed:

---
+++ bin/windows/init_sample.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+export LC_ALL=C
+
+script_basename="$(basename "$0")"
+script_dir="$(cd "$(dirname "$0")"; pwd)"
+script_path="${script_dir}"/"${script_basename}"
+cd "${script_dir}"
+start_time=$(date +%s.%N)
+
+# local.vim
+# cat << EOT | tee "${HOME}"/local.vim /mnt/c/work/local.vim
+# scriptencoding utf-8
+#
+# let g:my_bookmark_path = expand('~/work/bookmark.md')
+# let g:my_gtd_path = expand('~/work/gtd.md')
+# let g:local_vim_setting_name = fnamemodify(expand('<sfile>'), ":h:t")
+# EOT
+
+# [WSL2]
+# sudo mkdir -p /mnt/ad/
+# cat <<EOT | tee -a "${HOME}"/.bashrc
+# alias mntad='sudo mount -t drvfs '//ip/dir' /mnt/ad/'
+# cd
+# EOT
+
+# [Git for Windows]
+# add the followings to /etc/bash.bashrc
+# alias mntad='mount -t drvfs '//ip/dir' /mnt/ad/'
+
+# Git
+# git config --global user.email "${USER}"
+# git config --global user.name "${USER}"
+
+# setup
+sudo apt install -y \
+  bc \
+  make
+
+end_time=$(date +%s.%N)
+wall_time=$(echo "${end_time}"-"${start_time}" | bc -l)
+echo ["${script_path}"] WallTime: "${wall_time}" [s]
---
+++ bin/windows/my_copy.bat
@@ -0,0 +1,31 @@
+@echo off
+@REM Run as user.
+
+call :main
+exit /b
+
+:main
+    chcp 65001>nul
+    cd /d %~dp0
+
+    @REM %USERPROFILE%
+    copy /y ".\etc\home\dot.gitignore" "%USERPROFILE%\.gitignore"
+    copy /y ".\etc\home\dot.jupytext" "%USERPROFILE%\.jupytext"
+    copy /y ".\etc\windows\dot.wslconfig" "%USERPROFILE%\.wslconfig"
+
+    @REM Vim
+    set VIM_DIR="%USERPROFILE%\.vim"
+    rmdir /q /s "%VIM_DIR:"=%"
+    xcopy /e /i /y ".\dot.vim" "%VIM_DIR:"=%"
+
+    @REM VSCode
+    set CODE_DIR="%APPDATA%\Code\User"
+    rmdir /q /s "%CODE_DIR:"=%"
+    copy /y ".\etc\windows\code.settings.json "%CODE_DIR:"=%\settings.json"
+    xcopy /e /i /y ".\dot.vim\snippets\*" "%CODE_DIR:"=%\snippets"
+
+    @REM Windows Terminal
+    set WINTERM_DIR="%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+    rmdir /q /s "%WINTERM_DIR:"=%"
+    xcopy /e /i /y ".\etc\windows\wt.settings.json" "%WINTERM_DIR:"=%\settings.json"
+exit /b
