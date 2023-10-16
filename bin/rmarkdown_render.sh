d8f91215b975270bca0cc775f70dff0a6ba6e5ab
i9wa4 <127664533+i9wa4@users.noreply.github.com>
2023-10-16 17:41:43 +0900

    Changes to be committed:

--- bin/rmarkdown_render.sh
+++ bin/rmarkdown_render.sh
@@ -5,5 +5,5 @@ export LC_ALL=C
 find $1 -maxdepth 2 -name "*.Rmd" -type f -print | while read file
 do
   dir="$(dirname "${file}")"
-  R -e "rmarkdown::render(\""${file}"\", output_dir=\""${dir}"\")"
+  R -e "rmarkdown::render(\""${file}"\", output_dir=\""${dir}"\", output_format=\"all\")"
 done
