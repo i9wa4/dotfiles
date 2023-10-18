17aea6f98d71a24e5e59a333d45f17a24920e7bb
i9wa4 <127664533+i9wa4@users.noreply.github.com>
2023-10-18 14:31:25 +0900

    Update rmarkdown_render.sh

--- bin/rmarkdown_render.sh
+++ bin/rmarkdown_render.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 export LC_ALL=C
 
-find $1 -maxdepth 2 -name "*.Rmd" -type f -print | while read file
+find $1 -maxdepth 1 -name "*.Rmd" -type f -print | while read file
 do
   dir="$(dirname "${file}")"
   R -e "rmarkdown::render(\""${file}"\", output_dir=\""${dir}"\", output_format=\"all\")"
