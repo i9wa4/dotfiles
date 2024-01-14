130a11addaac83e6769bc2a79c198374a622fcee
i9wa4 <127664533+19wa4@users.noreply.github.com>
2024-01-14 21:36:33 +0900

    Changes to be committed:

--- bin/jl.sh
+++ bin/jl.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 
 rm -rf "${HOME}"/.ipynb_checkpoints
+rm -rf "${HOME}"/.virtual_documents
+cp -rf "${HOME}"/dotfiles/dot.config/jupyter/* "${PY_VENV_MYENV}"/share/jupyter
 cd $1
 jupyter-lab \
   --config="${XDG_CONFIG_HOME}"/jupyter/jupyter_lab_config.py \
