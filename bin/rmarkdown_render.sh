ecdad07d835df6a8a5496e49f48479153141fbc8
i9wa4 <127664533+19wa4@users.noreply.github.com>
2024-01-31 00:28:32 +0900

    Changes to be committed:

--- bin/rmarkdown_render.sh
+++ bin/rmarkdown_render.sh
@@ -5,10 +5,11 @@ find "$1" -maxdepth 1 -name "*.Rmd" -type f -print | while read -r filepath
 do
   dir=$(dirname "${filepath}")
   filename=$(basename "${filepath}")
-  cd "${dir}"
   cmd="rmarkdown::render('"
   cmd+="${filename}"
   cmd+="', output_dir='.', output_format='all')"
+
+  cd "${dir}"
   R -e "${cmd}"
   # R -e "rmarkdown::render(\""${filepath}"\", output_dir=\""${dir}"\", output_format=\"all\")"
 done
