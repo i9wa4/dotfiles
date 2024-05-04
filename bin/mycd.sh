b1530495aa7f1650a6c91beecf24bc562697ff0d
i9wa4 <127664533+19wa4@users.noreply.github.com>
2024-05-04 18:36:35 +0900

    Changes to be committed:

--- bin/mycd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-set -euo pipefail
-
-cd "$(dirname "$0")"
-echo "$(pwd)"
--- bin/windows/my_copy.bat
+++ bin/windows/my_copy.bat
@@ -9,25 +9,12 @@ exit /b
     cd /d %~dp0
 
     @REM %USERPROFILE%
-    @REM copy /y ".\etc\home\dot.gitignore" "%USERPROFILE%\.gitignore"
-    @REM copy /y ".\etc\home\dot.jupytext" "%USERPROFILE%\.jupytext"
     copy /y ".\etc\windows\dot.wslconfig" "%USERPROFILE%\.wslconfig"
-
-    @REM Vim
-    @REM set VIM_DIR="%USERPROFILE%\.vim"
-    @REM rmdir /q /s "%VIM_DIR:"=%"
-    @REM xcopy /e /i /y ".\dot.vim" "%VIM_DIR:"=%"
 
     @REM VSCode
     set CODE_DIR="%APPDATA%\Code\User"
     rmdir /q /s "%CODE_DIR:"=%"
     mkdir "%CODE_DIR:"=%\snippets"
-    copy /y ".\etc\windows\code.settings.json" "%CODE_DIR:"=%\settings.json"
+    copy /y ".\dot.vscode/settings.json" "%CODE_DIR:"=%\settings.json"
     xcopy /e /i /y ".\dot.vim\snippet_json" "%CODE_DIR:"=%\snippets"
-
-    @REM Windows Terminal
-    @REM set WINTERM_DIR="%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
-    @REM rmdir /q /s "%WINTERM_DIR:"=%"
-    @REM mkdir "%WINTERM_DIR:"=%"
-    @REM copy /y ".\etc\windows\wt.settings.json" "%WINTERM_DIR:"=%\settings.json"
 exit /b
