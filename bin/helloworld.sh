3083d63dc480e5e4ee409732c76d16595f7ca676
i9wa4 / uma-chan <127664533+i9wa4@users.noreply.github.com>
2024-06-18 13:48:56 +0900

    Changes to be committed:

--- bin/helloworld.sh
+++ bin/helloworld.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
-set -euox pipefail -o posix
+set -o verbose
+set -o xtrace
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o posix
 
-script_basename=$(basename "$0")
-script_dir=$(cd "$(dirname "$0")"; pwd)
-script_path="${script_dir}"/"${script_basename}"
-cd "${script_dir}"
-start_time=$(date +%s.%N)
+cd "$(dirname "$0")"
 
 echo "Hello, World!"
-# sleep 1
+sleep 1
 echo "Done."
-
-end_time=$(date +%s.%N)
-wall_time=$(echo "${end_time}"-"${start_time}" | bc -l)
-echo ["${script_path}"] WallTime: "${wall_time}" [s]
