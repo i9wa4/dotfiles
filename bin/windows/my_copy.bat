e6835ecd614274056be209640585412030a46059
i9wa4 <127664533+i9wa4@users.noreply.github.com>
2024-06-22 02:22:33 +0900

    Changes to be committed:

--- bin/windows/my_copy.bat
+++ bin/windows/my_copy.bat
@@ -17,4 +17,14 @@ exit /b
     mkdir "%CODE_DIR:"=%\snippets"
     copy /y ".\dot.vscode/settings.json" "%CODE_DIR:"=%\settings.json"
     xcopy /e /i /y ".\dot.vim\snippet" "%CODE_DIR:"=%\snippets"
+
+    @REM Alacritty
+    set CODE_DIR="%APPDATA%\alacritty"
+    rmdir /q /s "%CODE_DIR:"=%"
+    xcopy /e /i /y ".\dot.config\alacritty" "%CODE_DIR:"=%"
+    echo %APPDATA% >> "%CODE_DIR:"=%\alacritty.toml"
+    echo import = [ > "%CODE_DIR:"=%\alacritty.toml"
+    echo "%APPDATA%\alacritty\common.toml", >> "%CODE_DIR:"=%\alacritty.toml"
+    echo "%APPDATA%\alacritty\win.toml", >> "%CODE_DIR:"=%\alacritty.toml"
+    echo ] >> "%CODE_DIR:"=%\alacritty.toml"
 exit /b
