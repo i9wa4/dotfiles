fd9164cf9e07f772dc4662de80353cfbc8ac16d9
uma-chan <127664533+i9wa4@users.noreply.github.com>
2024-10-05 04:48:27 +0900

    new file:   bin/sk.py

---
+++ bin/sk.py
@@ -0,0 +1,13 @@
+import datetime
+import pyautogui
+import random
+import time
+
+loop = 0
+
+while True:
+    loop += 1
+    sleeptime = random.randint(200, 300)
+    pyautogui.press('numlock')
+    print(f'[{loop}] {datetime.datetime.now()} {sleeptime=}')
+    time.sleep(sleeptime)
