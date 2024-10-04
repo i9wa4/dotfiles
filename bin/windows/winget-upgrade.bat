d0c540ba7496228990bbe402c5808fea52f99366
uma-chan <127664533+i9wa4@users.noreply.github.com>
2024-10-04 13:23:55 +0900

    modified:   bin/windows/winget-upgrade.bat

--- bin/windows/winget-upgrade.bat
+++ bin/windows/winget-upgrade.bat
@@ -11,12 +11,12 @@ exit /b
 
     @REM winget settings --enable InstallerHashOverride
 
-    winget install --ignore-security-hash -e -id Alacritty.Alacritty
-    winget install --ignore-security-hash -e -id Amazon.Kindle
-    winget install --ignore-security-hash -e -id LINE.LINE
-    winget install --ignore-security-hash -e -id Microsoft.VisualStudioCode
-    winget install --ignore-security-hash -e -id Mozilla.Firefox
-    winget install --ignore-security-hash -e -id Zoom.Zoom.EXE
+    winget install --ignore-security-hash --extract --id Alacritty.Alacritty
+    winget install --ignore-security-hash --extract --id Amazon.Kindle
+    winget install --ignore-security-hash --extract --id LINE.LINE
+    winget install --ignore-security-hash --extract --id Microsoft.VisualStudioCode
+    winget install --ignore-security-hash --extract --id Mozilla.Firefox
+    winget install --ignore-security-hash --extract --id Zoom.Zoom.EXE
     winget upgrade --ignore-security-hash --all
 
     pause
