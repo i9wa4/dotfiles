905c02b5f66c99b660b17e8960083501ae6482f7
uma-chan <127664533+i9wa4@users.noreply.github.com>
2024-10-25 20:33:53 +0900

    modified:   bin/windows/win-copy.bat 	modified:   dot.config/alacritty/win.toml

--- bin/windows/win-copy.bat
+++ bin/windows/win-copy.bat
@@ -12,18 +12,18 @@ exit /b
     copy /y ".\etc\windows\dot.wslconfig" "%USERPROFILE%\.wslconfig"
 
     @REM Alacritty
-    set CODE_DIR="%APPDATA%\alacritty"
-    rmdir /q /s "%CODE_DIR:"=%"
-    xcopy /e /i /y ".\dot.config\alacritty" "%CODE_DIR:"=%"
-    echo %APPDATA% >> "%CODE_DIR:"=%\alacritty.toml"
-    echo import = [ > "%CODE_DIR:"=%\alacritty.toml"
-    echo "%APPDATA:\=/%/alacritty/common.toml", >> "%CODE_DIR:"=%/alacritty.toml"
-    echo "%APPDATA:\=/%/alacritty/win.toml", >> "%CODE_DIR:"=%/alacritty.toml"
-    echo ] >> "%CODE_DIR:"=%\alacritty.toml"
+    set ALACRITTY_DIR="%APPDATA%\alacritty"
+    rmdir /q /s "%ALACRITTY_DIR%"
+    xcopy /e /i /y ".\dot.config\alacritty" "%ALACRITTY_DIR%"
+    echo [general]>                                 "%ALACRITTY_DIR:"=%\alacritty.toml"
+    echo import = [>>                               "%ALACRITTY_DIR:"=%\alacritty.toml"
+    echo     "%ALACRITTY_DIR:\=/%/common.toml",>>   "%ALACRITTY_DIR:"=%\alacritty.toml"
+    echo     "%ALACRITTY_DIR:\=/%/win.toml", >>     "%ALACRITTY_DIR:"=%\alacritty.toml"
+    echo ]>>                                        "%ALACRITTY_DIR:"=%\alacritty.toml"
 
-    @REM VSCode
+    @REM VS Code
     set CODE_DIR="%APPDATA%\Code\User"
-    rmdir /q /s "%CODE_DIR:"=%"
+    rmdir /q /s "%CODE_DIR%"
     mkdir "%CODE_DIR:"=%\snippets"
     copy /y ".\dot.vscode\settings.json" "%CODE_DIR:"=%\settings.json"
     xcopy /e /i /y ".\dot.config\vim\snippet" "%CODE_DIR:"=%\snippets"
