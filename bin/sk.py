a6f4bc32c0d7aa72700abad1b143398115c43698
uma-chan <127664533+i9wa4@users.noreply.github.com>
2024-11-30 16:11:41 +0900

    modified:   bin/sk.py 	modified:   dot.config/vim/dpp/lazy.toml 	modified:   dot.config/vim/dpp/nvim.toml 	modified:   dot.config/vim/dpp/vim.toml 	modified:   dot.config/vim/rc/details.vim

--- bin/sk.py
+++ bin/sk.py
@@ -8,6 +8,6 @@ loop = 0
 while True:
     loop += 1
     sleeptime = random.randint(200, 300)
-    pyautogui.press('numlock')
-    print(f'[{loop}] {datetime.datetime.now()} {sleeptime=}')
+    pyautogui.press("numlock")
+    print(f"[{loop}] {datetime.datetime.now()} {sleeptime=}")
     time.sleep(sleeptime)
