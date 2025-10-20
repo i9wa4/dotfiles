3e387adb0e92a5cbadc83c77fc8823b5beea265e
uma-chan <127664533+i9wa4@users.noreply.github.com>
2025-10-20 15:18:30 +0900

    WIP

--- dot.config/alacritty/opacity-status.sh
+++ dot.config/alacritty/opacity-status.sh
@@ -5,11 +5,11 @@ set -o pipefail
 set -o posix
 
 OPACITY_FILE="$HOME/.config/alacritty/opacity.toml"
+DEFAULT_OPACITY=1.0
 
 if [[ -f $OPACITY_FILE ]]; then
   OPACITY=$(grep "^opacity" "$OPACITY_FILE" | sed 's/.*= *//')
-  PERCENT=$(echo "$OPACITY * 100" | bc | sed 's/\..*$//')
-  echo "[${PERCENT}%]"
+  echo "$OPACITY"
 else
-  echo "[80%]"
+  echo "$DEFAULT_OPACITY"
 fi
