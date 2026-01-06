549a1ec3a6e1168716bfcfba26ed784656e5c6ed
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-01-07 03:55:35 +0900

    WIP

--- nix/home.nix
+++ nix/home.nix
@@ -203,6 +203,7 @@ in {
       fi
     '';
 
+    # TODO: is this needed?
     # 2. Start ssh-agent if not running (Linux only)
     # cf. https://inno-tech-life.com/dev/infra/wsl2-ssh-agent/
     startSshAgent = lib.hm.dag.entryAfter ["writeBoundary"] (
