f8fc20bc06ae5db726b88a918a71744af5f1fe2d
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-02-19 21:01:19 +0900

    WIP

--- config/agents/scripts/statusline.sh
+++ config/agents/scripts/statusline.sh
@@ -6,9 +6,8 @@ set -o posix
 
 input=$(cat)
 
-# USED=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
+USED=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
 MODEL=$(echo "$input" | jq -r '.model.display_name // .model.id // "?"')
 VERSION=$(echo "$input" | jq -r '.version // "?"')
 
-# echo "${USED}% context used | ${MODEL} | v${VERSION}"
-echo "${MODEL} | v${VERSION}"
+echo "${USED}% context used | ${MODEL} | v${VERSION}"
