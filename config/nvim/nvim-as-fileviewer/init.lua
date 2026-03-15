4acc4dda321139f1bb77c926311a346eb20a7dc4
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-03-15 14:07:31 +0900

    feat(nvim): bootstrap oil.nvim in fileviewer profile

--- config/nvim/nvim-as-fileviewer/init.lua
+++ config/nvim/nvim-as-fileviewer/init.lua
@@ -1,2 +1,6 @@
+local path = vim.fn.stdpath("data") .. "/site/pack/plugins/start/oil.nvim"
+if not vim.uv.fs_stat(path) then
+  vim.fn.system({ "git", "clone", "https://github.com/stevearc/oil.nvim", path })
+end
 require("oil").setup()
 vim.keymap.set("n", "-", "<Cmd>Oil<CR>", { desc = "Open parent directory" })
