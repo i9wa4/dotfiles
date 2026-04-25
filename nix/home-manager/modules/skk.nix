d4521ef1c9e6311e3f5b105eed360f98742d1f74
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-04-25 19:02:15 +0900

    fix(skk): copy SKK-JISYO.L only to macSKK Dictionaries

--- nix/home-manager/modules/skk.nix
+++ nix/home-manager/modules/skk.nix
@@ -1,8 +1,6 @@
 {
   pkgs,
   lib,
-  dotfilesDir,
-  homeDir,
   ...
 }:
 {
@@ -10,17 +8,9 @@
   home.activation.setupSkkDicts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     ${lib.optionalString pkgs.stdenv.isDarwin ''
       macSkkDir="$HOME/Library/Application Support/macSKK/Dictionaries"
-      dppDir="''${XDG_CACHE_HOME:-$HOME/.cache}/dpp/repos/github.com"
+      dppSkkDev="''${XDG_CACHE_HOME:-$HOME/.cache}/dpp/repos/github.com/skk-dev/dict"
       mkdir -p "$macSkkDir"
-
-      for f in SKK-JISYO.L SKK-JISYO.jinmei SKK-JISYO.assoc; do
-        src="$dppDir/skk-dev/dict/$f"
-        [[ -f "$src" ]] && cp -f "$src" "$macSkkDir/$f"
-      done
-      src="$dppDir/uasi/skk-emoji-jisyo/SKK-JISYO.emoji.utf8"
-      [[ -f "$src" ]] && cp -f "$src" "$macSkkDir/SKK-JISYO.emoji.utf8"
-
-      [[ ! -f "$macSkkDir/skkdict.utf8" ]] && cp "${dotfilesDir}/config/vim/dpp/skkdict.utf8" "$macSkkDir/skkdict.utf8"
+      [[ -f "$dppSkkDev/SKK-JISYO.L" ]] && cp -f "$dppSkkDev/SKK-JISYO.L" "$macSkkDir/SKK-JISYO.L"
     ''}
   '';
 }
