7e9557db1507bbedd053c1f392768dc49a0e9bcf
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-04-25 18:52:58 +0900

    fix(skk): darwin-only macSKK copy, revert ddc.toml to dpp cache paths

--- nix/home-manager/modules/skk.nix
+++ nix/home-manager/modules/skk.nix
@@ -6,28 +6,21 @@
   ...
 }:
 {
-  # NOTE: dpp must run at least once before this activation produces full dict output.
+  # NOTE: dpp must run at least once before this activation produces dict output.
   home.activation.setupSkkDicts = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    skkDir="$HOME/.local/share/skk"
-    dppDir="''${XDG_CACHE_HOME:-$HOME/.cache}/dpp/repos/github.com"
-    mkdir -p "$skkDir"
-
-    for f in SKK-JISYO.L SKK-JISYO.jinmei SKK-JISYO.assoc; do
-      src="$dppDir/skk-dev/dict/$f"
-      [[ -f "$src" ]] && cp -f "$src" "$skkDir/$f"
-    done
-    src="$dppDir/uasi/skk-emoji-jisyo/SKK-JISYO.emoji.utf8"
-    [[ -f "$src" ]] && cp -f "$src" "$skkDir/SKK-JISYO.emoji.utf8"
-
-    [[ ! -f "$skkDir/skkdict.utf8" ]] && cp "${dotfilesDir}/config/vim/dpp/skkdict.utf8" "$skkDir/skkdict.utf8"
-
     ${lib.optionalString pkgs.stdenv.isDarwin ''
       macSkkDir="$HOME/Library/Application Support/macSKK/Dictionaries"
+      dppDir="''${XDG_CACHE_HOME:-$HOME/.cache}/dpp/repos/github.com"
       mkdir -p "$macSkkDir"
-      for f in SKK-JISYO.L SKK-JISYO.jinmei SKK-JISYO.assoc SKK-JISYO.emoji.utf8; do
-        [[ -f "$skkDir/$f" ]] && cp -f "$skkDir/$f" "$macSkkDir/$f"
+
+      for f in SKK-JISYO.L SKK-JISYO.jinmei SKK-JISYO.assoc; do
+        src="$dppDir/skk-dev/dict/$f"
+        [[ -f "$src" ]] && cp -f "$src" "$macSkkDir/$f"
       done
-      [[ ! -f "$macSkkDir/skkdict.utf8" ]] && [[ -f "$skkDir/skkdict.utf8" ]] && cp "$skkDir/skkdict.utf8" "$macSkkDir/skkdict.utf8"
+      src="$dppDir/uasi/skk-emoji-jisyo/SKK-JISYO.emoji.utf8"
+      [[ -f "$src" ]] && cp -f "$src" "$macSkkDir/SKK-JISYO.emoji.utf8"
+
+      [[ ! -f "$macSkkDir/skkdict.utf8" ]] && cp "${dotfilesDir}/config/vim/dpp/skkdict.utf8" "$macSkkDir/skkdict.utf8"
     ''}
   '';
 }
