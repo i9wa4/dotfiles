012405223e5271845e9baf52bc37d426aa36c6b9
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-05-10 19:54:24 +0900

    refactor(nvim): simplify init helpers and highlight handling

--- config/nvim/nvim/init.lua
+++ config/nvim/nvim/init.lua
@@ -5,11 +5,19 @@ vim.cmd("syntax off")
 -- --------------------------------------
 -- Helper
 --
-local api = vim.api
-local fn = vim.fn
-local opt = vim.opt
+local function send_tmux_clipboard(content)
+  if vim.fn.executable("tmux") == 1 and vim.env.TMUX and vim.env.TMUX ~= "" then
+    vim.system({ "tmux", "load-buffer", "-w", "-" }, { stdin = content }):wait()
+  end
+end
 
-api.nvim_create_user_command("R", function(opts)
+local function send_register(reg)
+  local content = vim.fn.getreg(reg)
+  pcall(vim.fn.setreg, "+", content)
+  send_tmux_clipboard(content)
+end
+
+vim.api.nvim_create_user_command("R", function(opts)
   send_register(opts.args)
 end, { nargs = "?" })
 
@@ -31,12 +39,55 @@ local function lnext()
   end
 end
 
+local function highlight_define()
+  vim.api.nvim_set_hl(0, "HlMS", { bg = "#FFB6C1", fg = "#000000" })
+  vim.api.nvim_set_hl(0, "markdownError", { link = "Normal" })
+  vim.api.nvim_set_hl(0, "markdownItalic", { link = "Normal" })
+
+  for _, group in ipairs({
+    "EndOfBuffer",
+    "Folded",
+    "Identifier",
+    "LineNr",
+    "NonText",
+    "Normal",
+    "Special",
+    "StatusLine",
+    "StatusLineNC",
+    "VertSplit",
+  }) do
+    local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
+    hl.bg = nil
+    vim.api.nvim_set_hl(0, group, hl)
+  end
+end
+
+local function highlight_match()
+  for _, id in ipairs(vim.w.my_highlight_match_ids or {}) do
+    pcall(vim.fn.matchdelete, id)
+  end
+
+  local ids = {}
+  local function add(group, pattern)
+    ids[#ids + 1] = vim.fn.matchadd(group, pattern)
+  end
+
+  add("HlMS", "HlMS")
+  add("HlMS", [[TODO:\|FIXME:\|DEBUG:\|NOTE:\|WARNING:\|HACK:\|# %%\|\[ \]])
+  local now = vim.fn.localtime()
+  add("HlMS", vim.fn.strftime("%Y%m%d", now))
+  add("HlMS", vim.fn.strftime("%Y-%m-%d", now))
+  add("HlMS", vim.fn.strftime("%Y/%m/%d", now))
+
+  vim.w.my_highlight_match_ids = ids
+end
+
 local function toggle_quote(line1, line2)
   if line2 < line1 then
     line1, line2 = line2, line1
   end
 
-  local lines = api.nvim_buf_get_lines(0, line1 - 1, line2, false)
+  local lines = vim.api.nvim_buf_get_lines(0, line1 - 1, line2, false)
   local all_quoted = true
   for _, line in ipairs(lines) do
     if not line:match("^>") then
@@ -54,71 +105,7 @@ local function toggle_quote(line1, line2)
     end
   end
 
-  api.nvim_buf_set_lines(0, line1 - 1, line2, false, new_lines)
-end
-
-local function send_tmux_clipboard(content)
-  if fn.executable("tmux") == 1 and vim.env.TMUX and vim.env.TMUX ~= "" then
-    vim.system({ "tmux", "load-buffer", "-w", "-" }, { stdin = content }):wait()
-  end
-end
-
-local function send_register(reg)
-  local content = fn.getreg(reg)
-  pcall(fn.setreg, "+", content)
-  send_tmux_clipboard(content)
-end
-
-local function highlight_define()
-  api.nvim_set_hl(0, "HlMS", { bg = "#FFB6C1", fg = "#000000" })
-  api.nvim_set_hl(0, "markdownError", { link = "Normal" })
-  api.nvim_set_hl(0, "markdownItalic", { link = "Normal" })
-
-  local function clear_bg(group)
-    local hl = api.nvim_get_hl(0, { name = group, link = false })
-    hl.bg = nil
-    api.nvim_set_hl(0, group, hl)
-  end
-
-  for _, group in ipairs({
-    "EndOfBuffer",
-    "Folded",
-    "Identifier",
-    "LineNr",
-    "NonText",
-    "Normal",
-    "Special",
-    "StatusLine",
-    "StatusLineNC",
-    "VertSplit",
-  }) do
-    clear_bg(group)
-  end
-end
-
-local function delete_highlight_matches()
-  for _, id in ipairs(vim.w.my_highlight_match_ids or {}) do
-    pcall(fn.matchdelete, id)
-  end
-  vim.w.my_highlight_match_ids = {}
-end
-
-local function highlight_match()
-  delete_highlight_matches()
-
-  local ids = {}
-  local function add(pattern)
-    ids[#ids + 1] = fn.matchadd("HlMS", pattern)
-  end
-
-  add("HlMS")
-  add([[TODO:\|FIXME:\|DEBUG:\|NOTE:\|WARNING:\|HACK:\|# %%\|\[ \]])
-  local now = fn.localtime()
-  add(fn.strftime("%Y%m%d", now))
-  add(fn.strftime("%Y-%m-%d", now))
-  add(fn.strftime("%Y/%m/%d", now))
-
-  vim.w.my_highlight_match_ids = ids
+  vim.api.nvim_buf_set_lines(0, line1 - 1, line2, false, new_lines)
 end
 
 -- --------------------------------------
@@ -128,16 +115,16 @@ local map = vim.keymap.set
 
 map("n", "gf", "gF")
 map("n", "gqq", function()
-  toggle_quote(fn.line("."), fn.line("."))
+  toggle_quote(vim.fn.line("."), vim.fn.line("."))
 end)
 map("v", "gqq", function()
-  toggle_quote(fn.line("v"), fn.line("."))
+  toggle_quote(vim.fn.line("v"), vim.fn.line("."))
 end)
 map("i", ",now", function()
-  return fn.strftime("%Y-%m-%d %X +0900")
+  return vim.fn.strftime("%Y-%m-%d %X +0900")
 end, { expr = true })
 map("i", ",today", function()
-  return fn.strftime("%Y-%m-%d")
+  return vim.fn.strftime("%Y-%m-%d")
 end, { expr = true })
 map("n", "<C-p>", lprevious)
 map("n", "<C-n>", lnext)
@@ -149,25 +136,25 @@ map("n", "<Space>sw", "<Cmd>setlocal wrap! wrap?<CR>")
 -- --------------------------------------
 -- Autocmd
 --
-local augroup = api.nvim_create_augroup("MyAutocmd", { clear = true })
+local augroup = vim.api.nvim_create_augroup("MyAutocmd", { clear = true })
 
-api.nvim_create_autocmd("BufReadPost", {
+vim.api.nvim_create_autocmd("BufReadPost", {
   group = augroup,
   callback = function()
     pcall(vim.cmd, [[normal! g`"]])
   end,
 })
 
-api.nvim_create_autocmd("BufEnter", {
+vim.api.nvim_create_autocmd("BufEnter", {
   group = augroup,
   callback = function()
     if vim.bo.buftype == "" then
-      fn.setreg("a", fn.fnamemodify(fn.expand("%"), ":p:~"))
+      vim.fn.setreg("a", vim.fn.fnamemodify(vim.fn.expand("%"), ":p:~"))
     end
   end,
 })
 
-api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd("FileType", {
   group = augroup,
   callback = function()
     if vim.wo.diff then
@@ -179,80 +166,80 @@ api.nvim_create_autocmd("FileType", {
   end,
 })
 
-api.nvim_create_autocmd("TextYankPost", {
+vim.api.nvim_create_autocmd("TextYankPost", {
   group = augroup,
   callback = function()
     if vim.v.event.operator == "y" and vim.v.event.regname == "" then
-      send_tmux_clipboard(fn.getreg(""))
+      send_tmux_clipboard(vim.fn.getreg(""))
     end
   end,
 })
 
-api.nvim_create_autocmd("ColorScheme", {
+vim.api.nvim_create_autocmd("ColorScheme", {
   group = augroup,
   callback = highlight_define,
 })
 
-api.nvim_create_autocmd({ "WinEnter", "FileType" }, {
+vim.api.nvim_create_autocmd({ "WinEnter", "FileType" }, {
   group = augroup,
   callback = highlight_match,
 })
 
-vim.g.auto_reload = fn.timer_start(1000, function()
+vim.g.auto_reload = vim.fn.timer_start(1000, function()
   vim.cmd("silent! checktime")
 end, { ["repeat"] = -1 })
 
 -- --------------------------------------
 -- Option
 --
-opt.hidden = true
-opt.nrformats = { "unsigned" }
+vim.opt.hidden = true
+vim.opt.nrformats = { "unsigned" }
 
-if fn.executable("rg") == 1 then
-  opt.grepprg = "rg --vimgrep --no-heading"
-  opt.grepformat = "%f:%l:%c:%m"
+if vim.fn.executable("rg") == 1 then
+  vim.opt.grepprg = "rg --vimgrep --no-heading"
+  vim.opt.grepformat = "%f:%l:%c:%m"
 end
 
-opt.hlsearch = true
-opt.ignorecase = true
-opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
 pcall(function()
-  opt.shortmess:remove("S")
+  vim.opt.shortmess:remove("S")
 end)
-opt.smartcase = true
-opt.wrapscan = true
+vim.opt.smartcase = true
+vim.opt.wrapscan = true
 
-opt.ambiwidth = "double"
-opt.cursorline = true
-opt.list = true
-opt.listchars = { space = "␣", tab = ">-", trail = "~", nbsp = "%" }
-opt.number = true
+vim.opt.ambiwidth = "double"
+vim.opt.cursorline = true
+vim.opt.list = true
+vim.opt.listchars = { space = "␣", tab = ">-", trail = "~", nbsp = "%" }
+vim.opt.number = true
 
-vim.g.netrw_home = fn.expand("$XDG_CACHE_HOME")
-opt.autoread = true
-opt.backup = false
-opt.swapfile = false
-opt.undofile = false
-opt.clipboard = ""
+vim.g.netrw_home = vim.fn.expand("$XDG_CACHE_HOME")
+vim.opt.autoread = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.undofile = false
+vim.opt.clipboard = ""
 
-if fn.has("mac") == 1 or fn.has("macunix") == 1 then
-  opt.clipboard:prepend("unnamed")
+if vim.fn.has("mac") == 1 or vim.fn.has("macunix") == 1 then
+  vim.opt.clipboard:prepend("unnamed")
 else
-  opt.clipboard:prepend("unnamedplus")
+  vim.opt.clipboard:prepend("unnamedplus")
 end
 
-if fn.has("termguicolors") == 1 then
-  opt.termguicolors = true
+if vim.fn.has("termguicolors") == 1 then
+  vim.opt.termguicolors = true
 end
 
 -- --------------------------------------
 -- Plugin
 --
-local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
   vim.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim", lazypath }):wait()
 end
-opt.runtimepath:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
   {
