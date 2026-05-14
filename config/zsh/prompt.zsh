d49f6c6e2e85b2c708f8b624f9c0733c4c314967
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-05-15 00:24:45 +0900

    feat(prompt): show lock-safe repo status

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -11,10 +11,10 @@ else
 fi
 
 function _get_simplified_path() {
-  local path="${PWD}"
-  path="${path/#$HOME\/ghq\/github.com\//}"
-  path="${path/#$HOME/~}"
-  echo "${path}"
+  local display_path="${PWD}"
+  display_path="${display_path/#$HOME\/ghq\/github.com\//}"
+  display_path="${display_path/#$HOME/~}"
+  echo "${display_path}"
 }
 
 function _get_devshell_indicator() {
@@ -35,9 +35,15 @@ function _get_context_line() {
   print -n "${remote}${devshell}${aws}"
 }
 
+function _get_repo_status() {
+  local repo_status_text
+  repo_status_text="$(command repo-status 2>/dev/null)"
+  [[ -n "${repo_status_text}" ]] && print -n " ${repo_status_text}"
+}
+
 precmd() {
-  # _refresh_repo_status
   local context="$(_get_context_line)"
-  local path="$(_get_simplified_path)"
-  PROMPT=$'\n'"${context}[%D{%Y-%m-%d %H:%M:%S}] %S[${path}]%s"$'\n$ '
+  local display_path="$(_get_simplified_path)"
+  local repo_status="$(_get_repo_status)"
+  PROMPT=$'\n'"${context}[%D{%Y-%m-%d %H:%M:%S}] %S[${display_path}]%s${repo_status}"$'\n$ '
 }
