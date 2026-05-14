7791de6c2342ce63110e71be6adcc74b4e57a142
uma-chan <127664533+i9wa4@users.noreply.github.com>
2026-05-15 01:05:31 +0900

    refactor(zsh): simplify prompt context indicators

--- config/zsh/prompt.zsh
+++ config/zsh/prompt.zsh
@@ -3,13 +3,6 @@
 
 setopt prompt_subst
 
-# SSH connection detection
-if [[ -n "${SSH_CONNECTION}" || -n "${SSH_TTY}" || -n "${SSH_CLIENT}" ]]; then
-  _IS_LOCAL=0
-else
-  _IS_LOCAL=1
-fi
-
 function _get_simplified_path() {
   local display_path="${PWD}"
   display_path="${display_path/#$HOME\/ghq\/github.com\//}"
@@ -17,22 +10,13 @@ function _get_simplified_path() {
   echo "${display_path}"
 }
 
-function _get_devshell_indicator() {
-  if [[ -n "${IN_NIX_SHELL}" || -n "${DIRENV_DIR}" ]]; then
-    print -n "💠"
-  fi
-}
-
 function _get_aws_profile_indicator() {
   [[ -n "${MY_AWS_PROFILE}" ]] && print -n "🔶${MY_AWS_PROFILE}"
 }
 
 function _get_context_line() {
-  local remote=""
-  [[ "${_IS_LOCAL}" -eq 0 ]] && remote="🌐"
-  local devshell="$(_get_devshell_indicator)"
   local aws="$(_get_aws_profile_indicator)"
-  print -n "${remote}${devshell}${aws}"
+  print -n "${aws}"
 }
 
 function _get_repo_status() {
